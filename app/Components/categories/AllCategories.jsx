91c740da205641c22fa23c23511a9766878a473e
shanumukurao <rsaikumar147@gmail.com>
2026-02-04 18:44:14 +0530

    categories resolved

--- app/Components/categories/AllCategories.jsx
+++ app/Components/categories/AllCategories.jsx
@@ -1,7 +1,7 @@
 `use client`;
 
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const AllCategories = () => {
