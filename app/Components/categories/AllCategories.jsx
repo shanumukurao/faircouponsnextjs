d3fd2f4b06a5d610f8d0fbc0a395951272a1bafb
shanumukurao <rsaikumar147@gmail.com>
2026-02-04 18:39:16 +0530

    categories added

--- app/Components/categories/AllCategories.jsx
+++ app/Components/categories/AllCategories.jsx
@@ -1,7 +1,7 @@
 `use client`;
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import React from "react";
 
 const AllCategories = () => {
